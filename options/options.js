b8326d14c1f4c29fab755900674acd7fabe7531d
Crowdbotics <team@crowdbotics.com>
2021-08-04 13:39:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "shy_meadow_29448Identifier",
+  url: "https://shy_meadow_29448Identifier.botics.co",
+  api: "https://shy_meadow_29448Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
